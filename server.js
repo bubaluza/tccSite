cc330adb6bfa4e19fa41cc8ae7d0a8811ed0cafc
Bubaluza <gabrielschiller8@gmail.com>
2019-09-08 23:43:30 -0300

    v1

--- server.js
+++ server.js
@@ -12,8 +12,10 @@ var parsejson;
 let i, maior, result;
 let po=[], ponto=[], pontos=[], chav0=[], chav1=[];//vetores
 let Vth, Voc, Isc, Vmp, Imp, Pmp, n, Io, Rso, Rsho, Rs, Rsh, Iph, FF, S0, S1, C;
-let pcVoc = 0.9, pcIsc = 0.95, pcS0i = 0.8, pcS0f = 1, pcS1i = 0, pcS1f = 0.1;
+let pcVoc = 0.9, pcIsc = 0.95, pcS0i = 0.5, pcS0f = 0.8, pcS1i = 0, pcS1f = 0.6;
 let potencia;
+let progresso;
+let erro=false;
 
 
 port.pipe(parser);
@@ -29,21 +31,37 @@ app.listen(8080, function() {
 app.get('/ajaxSet', (req, res) => {
         resposta = {};
         resposta.chegou = false;
-        port.write('a');
+        port.write('r');
+        progresso=0;
         res.send(resposta);
 });
 
 app.get('/ajaxMain', (req, res) => {
-    if(dadoPronto){
+    if(erro){
+        resposta={};
+        resposta.erro=true;
+        erro=false;
+        res.send(resposta);
+    }else if(dadoPronto){
         resposta = {};
         resposta.chegou = true;
         resposta.dadosIv = parsejson;
         resposta.pot = potencia;
+        resposta.din = parsejson2;
+        resposta.maxima_potencia =Pmp;
+        resposta.tensao_maxima_potencia =Vmp;
+        resposta.corrente_maxima_potencia =Imp;
+        resposta.corrente_cc =Isc;
+        resposta.tensao_ca =Voc;
+        resposta.rs =Rs;
+        resposta.rp =Rsh;
+        resposta.cd =C;
         dadoPronto=false;
         res.send(resposta);
     }else{
         resposta = {};
         resposta.chegou = false;
+        resposta.progresso = progresso;
         res.send(resposta);
     }
 });
@@ -53,53 +71,56 @@ parser.on('data', line=> {
     const json = JSON.parse(line);
     console.log(json);
     if(json.data == true){
-        po=[];
-        potencia=[];
-        chav0=[];
-        chav1=[];
-        pontos=[];
-        parsejson = json.dataIv;
-        parsejson2 = json.dataDin;
-        for(let k=0;k<parsejson.length-1;k++){
-            if(parsejson[k].voltage==-1){
-                parsejson =parsejson.slice(0, k);
-                break;
+        try {
+            po = [];
+            potencia = [];
+            chav0 = [];
+            chav1 = [];
+            pontos = [];
+            parsejson = json.dataIv;
+            parsejson2 = json.dataDin;
+            for (let k = 0; k < parsejson.length - 1; k++) {
+                if (parsejson[k].voltage == -1) {
+                    parsejson = parsejson.slice(0, k);
+                    break;
+                }
             }
-        }
 
-        for(i = 0; i < parsejson.length-1; i++)//calculo potencia
-            po[i] = parsejson[i].voltage*parsejson[i].current
-        for(i = 0; i < parsejson.length-1; i++){
-            potencia[i] = {};
-            potencia[i].voltage =parsejson[i].voltage;
-            potencia[i].power = po[i];
+            for (i = 0; i < parsejson.length - 1; i++)//calculo potencia
+                po[i] = parsejson[i].voltage * parsejson[i].current
+            for (i = 0; i < parsejson.length - 1; i++) {
+                potencia[i] = {};
+                potencia[i].voltage = parsejson[i].voltage;
+                potencia[i].power = po[i];
+            }
+            i = 0;
+            while (parsejson2[i].switching == 0) {
+                chav0[i] = parsejson2[i];
+                i++;
+            }
+            let j = 0;
+            for (i; i < parsejson2.length; i++) {
+                chav1[j] = parsejson2[i];
+                j++;
+            }
+            Voc = parsejson[0].voltage;
+            Isc = parsejson[parsejson.length - 1].current;
+            Vth = 0.026;
+            console.log(Voc);
+            console.log(Isc);
+            extractRes();
+            extractCap();
+            dadoPronto = true;
+        } catch (e){
+            console.log(e);
+            erro = true;
         }
-        i = 0;
-        while(parsejson2[i].switching==0){
-            chav0[i] = parsejson2[i];
-            i++;
-        }
-        let j = 0;
-        for(i; i<parsejson2.length; i++){
-            chav1[j] = parsejson2[i];
-            j++;
-        }
-        Voc = parsejson[0].voltage;
-        Isc = parsejson[parsejson.length-1].current;
-        Vth = 0.026;
-        console.log(Voc);
-        console.log(Isc);
-        dadoPronto = true;
-        extractRes();
-
+    }else if(json.progresso>=0){
+        progresso = json.progresso;
+    }else if(json.terminouIv == true){
+        port.write('a');
     }
 
-    // if(parsejson.terminouIv == true){
-    //     port.write('a');
-    //     dadoPronto = true;
-    // }else if(parsejson.terminouData ==true){
-    //
-    // }
 });
 
 
@@ -115,8 +136,8 @@ function extractRes(){
         console.log(parsejson[i].voltage+" "+parsejson[i].current);
     }
     result = regression.linear(pontos, {precision: 5});
-    Rsho = -1/result.equation[0];
-    console.log("Rpo: "+Rsho);
+    Rso = -1/result.equation[0];
+    console.log("Rso: "+Rso);
     pontos=[];
     console.log("\nTensao x Corrente para 10% Isc");
     for(i=parsejson.length-1; parsejson[i].current > (Isc*pcIsc) ; i--){
@@ -127,44 +148,51 @@ function extractRes(){
         console.log(parsejson[i].voltage+" "+parsejson[i].current);
     }
     result = regression.linear(pontos, {precision: 5});
-    Rso = -1/result.equation[0];
-    console.log("Rso: "+Rso);
+    Rsho = -1/result.equation[0];
+    console.log("Rsho: "+Rsho);
     calcPmp();
     calcCelik();
 }
 
 function extractCap(){
     console.log("\nTempo x Tensão");
-    for(i = 0; i < chav0.length*pcS0i; i++);
-    for(i; i < chav0.length*pcS0f; i++){
+    pontos=[];
+    console.log("S0");
+    for(i = 0; i < (chav0.length)*pcS0i; i++);
+    for(i; i < (chav0.length-1)*pcS0f; i++){
+        let ponto=[];
         ponto[0] = chav0[i].time;
         ponto[1] = chav0[i].voltage;
-        pontos.push(ponto.slice(0));
+        pontos.push(ponto);
         console.log(chav0[i].time+" "+chav0[i].voltage);
     }
     result = regression.linear(pontos, {precision: 5});
     S0 = result.equation[0];
-    for(i = 0; i < chav1.length*pcS1f; i++){
+    pontos=[];
+    console.log("S1");
+    for(i = 0; i < (chav1.length)*pcS1f; i++){
+        let ponto=[];
         ponto[0] = chav1[i].time;
         ponto[1] = chav1[i].voltage;
-        pontos.push(ponto.slice(0));
+        pontos.push(ponto);
         console.log(chav1[i].time+" "+chav1[i].voltage);
     }
     result = regression.linear(pontos, {precision: 5});
     S1 = result.equation[0];
+    console.log("S0 "+S0+"S1 "+S1);
     C = (-S0*0.000022)/(S0-S1);
     console.log("Capacitância interna: "+C);
 }
 
 function calcPmp(){
-    for(i = 0; i < parsejson.length-1; i++){
+    for(i = 0; i < parsejson.length; i++){
         if(po[i] > po[i-1])
             maior = i;
-        else break;
     }
-    Vmp = parsejson[i].voltage;
-    Imp = parsejson[i].current;
+    Vmp = parsejson[maior].voltage;
+    Imp = parsejson[maior].current;
     Pmp = po[maior];
+    console.log('potencia: '+Pmp);
 }
 
 function calcCelik(){
